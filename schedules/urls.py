97a186f8bda29a8068d87160ffad09dc63cb0dc3
merino <merinoeug118@gmail.com>
2025-06-16 22:10:47 -0600

    mas error

--- schedules/urls.py
+++ schedules/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import register_attendance
 
 urlpatterns = [
-    path('api/attendance/', register_attendance, name='register-attendance'),
+    path('attendance/', register_attendance, name='register-attendance'),
 ]
